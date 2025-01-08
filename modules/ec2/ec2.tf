e33c4d7fbc035d750cdfc9f6304c708b23286882
khushpardhi <128688910+khushpardhi@users.noreply.github.com>
2025-01-08 17:04:16 +0530

    Update ec2.tf

--- modules/ec2/ec2.tf
+++ modules/ec2/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "my_ec2" {
   vpc_security_group_ids = [
     var.ec2_sg_id
   ]
-  user_data = file("deploy.sh")
+  user_data = file("../module/deploy.sh")
   # # User Data for deployment
   # user_data = <<-EOF
               # #!/bin/bash
