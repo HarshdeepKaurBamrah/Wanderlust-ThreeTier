48540a950fc4af5616f241fc0e6225cf9df99a3f
khushpardhi <128688910+khushpardhi@users.noreply.github.com>
2025-01-08 19:29:47 +0530

    Update deploy.sh

--- modules/ec2/deploy.sh
+++ modules/ec2/deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 echo "Updating the system..."
-sudo apt-get update -y
-
-cd /tmp
+apt-get update -y
 
 echo "Cloning the repository..."
 git clone https://github.com/khushpardhi/wanderlust.git /home/ubuntu/wanderlust
@@ -10,19 +8,20 @@ git clone https://github.com/khushpardhi/wanderlust.git /home/ubuntu/wanderlust
 cat tmp
 
 echo "Installing dependencies..."
-sudo apt install -y software-properties-common python3-pip
+apt install -y software-properties-common python3-pip
 
 echo "Adding Ansible PPA..."
-sudo add-apt-repository ppa:ansible/ansible -y
+add-apt-repository ppa:ansible/ansible -y
 
 echo "Updating package list again..."
-sudo apt update
+apt-get update -y
 
 echo "Installing Ansible..."
-sudo apt install -y ansible
+apt install -y ansible
 
 echo "Verifying Ansible installation..."
 ansible --version
 
-cd wanderlust/ansible/
+cd /home/ubuntu/wanderlust/ansible
+
 ansible-playbook main.yml
