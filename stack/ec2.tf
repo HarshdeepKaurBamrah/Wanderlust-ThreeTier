1ae638c6f1dc409cb877fa538a0008aa931ff7cf
khushpardhi <128688910+khushpardhi@users.noreply.github.com>
2025-01-09 15:05:12 +0530

    Update ec2.tf

--- stack/ec2.tf
+++ stack/ec2.tf
@@ -1,5 +1,5 @@
 module "ec2" {
-  source = "../module/ec2/"
+  source = "/module/ec2/"
   instance_type = "t2.micro" 
   ec2_sg_id = module.vpc.security_group_id 
   ec2_subnet_id = module.vpc.subnet_id  
