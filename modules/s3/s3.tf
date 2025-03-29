8157317765d0a9884fbc02accf45b02d2d4c522d
khushpardhi <pardhikhush@gmail.com>
2025-03-29 11:29:47 +0530

    devops

--- modules/s3/s3.tf
+++
@@ -1,5 +0,0 @@
-# resource "aws_s3_object" "object" {
-#   bucket = "terastate1"
-#   key    = "terraform.tfstate"
-#   source = "/home/ncs/khush/vpc/terraform.tfstate"
-# }
--- stack/output.tf
+++
@@ -1,6 +0,0 @@
-output "subnet_id" {
-  value       = module.vpc.subnet_id  
-}
-output "security_group_id" {
-  value       = module.vpc.security_group_id 
-}
